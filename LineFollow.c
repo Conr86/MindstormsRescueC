6e87e5c3589e1ccd11f1b90e4c5c20d17fef9147
Connor <conno123009@programmer.net>
2014-10-27 16:08:50 +1100

    Added comments and general code cleanup

--- LineFollow.c
+++ LineFollow.c
@@ -5,55 +5,56 @@
 #pragma config(Motor,  motorD,          Right,         tmotorEV3_Large, PIDControl, encoder)
 
 //Setup Motor Speeds
-int motorSpeed = 30;
-int reverseSpeed = -5;
+int motorSpeed = 30;			//Adjustable, preferably above 20 and below 50
+int reverseSpeed = -5; 			//This is only used in turning.
 
 //Temporary values for colours.
-int colourBlackMin = 0;
-int colourBlackMax = 16;
+int colourBlackMin = 0;			//Lowest possible value (Darkest / Black)
+int colourBlackMax = 16;		//This value changes in calibration
 
-int colourGreenMin = 16;
-int colourGreenMax = 20;
+int colourGreenMin = 16;		//Needs to be adjusted, it may conflict with colourBlackMax
+int colourGreenMax = 20;		//This value changes in calibration
 
-int colourWhiteMin = 20;
-int colourWhiteMax = 90;
+int colourWhiteMin = 20;		//Value needs to be adjusted, possibly higher value
+int colourWhiteMax = 90;		//This value changes in calibration
 
-int colourFoilMin = 90;
+int colourFoilMin = 90;			//Doesn't need to be calibrated. Could even be raised to '95'
 
 
-//Setup integers for current light sensor values
-int colourSensorLeft;
-int colourSensorRight;
-
+//Setup variables for current light sensor values
+int colourSensorLeft;			//Constantly updates. Holds current S1 value (0-100)
+int colourSensorRight;			//Constantly updates. Holds current S2 value (0-100)
 task setup() {
-	//Calibrate black by working out average of two light sensor values plus 5.
-	waitUntil(getTouchValue(S4) == 1);
-	playSound(soundBlip);
-	colourBlackMax = SensorValue(LightLeft) + SensorValue(LightRight) / 2 + 5;
-	delay(500);
+	//Set black variable by working out average of the two light sensor values plus 5
+  colourSensorLeft = SensorValue(LightLeft);
+	colourSensorRight = SensorValue(LightRight);
+	waitUntil(getTouchValue(S4) == 1);	//Wait until Touch sensor is pressed
+	playSound(soundBlip);			//Plays simple 'Beep' sound.
+	colourBlackMax = colourSensorLeft + colourSensorRight / 2 + 5;
+	delay(500);				//Wait 500 milliseconds
 
 	//Calibrate white by working out average of two light sensor values minus 5.
-	waitUntil(getTouchValue(S4) == 1);
-	playSound(soundBlip);
+	waitUntil(getTouchValue(S4) == 1);  	//Wait until touch sensor is pressed
+	playSound(soundBlip);			//Plays simple 'Beep' sound.
 	colourWhiteMin = SensorValue(LightLeft) + SensorValue(LightRight) / 2 - 5;
-	delay(500);
+	delay(500);				//Wait 500 Milliseconds
 }
 
-task chemicalSpill() {
-	playSound(soundLowBuzz);
-	while (true){
+task chemicalSpill() {				//Begin Chemical Spill mode
+	playSound(soundLowBuzz);		//Play 'buzz' sound
+	while (true){				//Create new loop, it will never exit this.
 		//Get to the chopper!
 	}
 }
 
 task main(){
-	startTask(setup);
-	while (true){
+	startTask(setup);			//Run setup
+	while (true){				//Loop
 		colourSensorLeft = SensorValue(LightLeft);
 		colourSensorRight = SensorValue(LightRight);
 
 		if (colourSensorLeft > colourBlackMin && colourSensorLeft < colourBlackMax) {
-			motor[Left] = motorSpeed;
+			motor[Left] = motorSpeed;				
 			motor[Right] = reverseSpeed;
 			//Turn Left on Black
 		}
